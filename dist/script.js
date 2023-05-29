e4cee12e552ca2262d2560401213b3c4b0b756be
Kabeya ilunga <97855043+Kabeya243@users.noreply.github.com>
2023-05-29 19:15:03 +0200

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+AOS.init();
+// sliders script 
+// sliders script 
+let themeBtn = document.querySelector(".bx-sun");
+themeBtn.addEventListener("click",() => {
+    document.body.classList.toggle("dark");
+})
